95ecefd9afd236c21d0c9d7cb885597150851983
José García <garcia.josea@pucp.edu.pe>
2021-06-21 20:20:27 -0500

    -

--- src/main/java/com/example/lab9/repository/UsuarioReposity.java
+++ src/main/java/com/example/lab9/repository/UsuarioReposity.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UsuarioReposity extends JpaRepository<Usuario, Integer> {
+public interface UsuarioReposity extends JpaRepository<Usuario, String> {
 }
