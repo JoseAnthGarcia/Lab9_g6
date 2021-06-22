1f2d83a14967a2afba29d7c65bab157e0235dbe5
José García <garcia.josea@pucp.edu.pe>
2021-06-22 00:18:35 -0500

    -

--- src/main/java/com/example/lab9/controller/ActividadControllerWS.java
+++ src/main/java/com/example/lab9/controller/ActividadControllerWS.java
@@ -1,16 +1,49 @@
 package com.example.lab9.controller;
 
+import com.example.lab9.entity.Actividad;
+import com.example.lab9.entity.Usuario;
+import com.example.lab9.repository.ActividadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 @RestController
 public class ActividadControllerWS {
 
-    @GetMapping(value = "/actividad",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity listaActividades(){
-        return new ResponseEntity("hola", HttpStatus.OK);
+    @Autowired
+    ActividadRepository actividadRepository;
+
+    @GetMapping(value = "/actividad", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity listaActividades() {
+        return new ResponseEntity(actividadRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/actividad", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity crearUusario(@RequestBody Actividad actividad,
+                                       @RequestParam(value = "fetchId") boolean fetchCorreo) {
+        HashMap<String, Object> responseMap = new HashMap<>();
+        actividadRepository.save(actividad);
+        responseMap.put("estado", "creado");
+        if (fetchCorreo) {
+            responseMap.put("id", actividad.getIdactividad());
+        }
+        return new ResponseEntity(responseMap, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity capturarExcepcion(HttpServletRequest httpServletRequest){
+
+        HashMap<String, String> responseMap = new HashMap<>();
+        if(httpServletRequest.getMethod().equals("POST") || httpServletRequest.getMethod().equals("PUT")){
+            responseMap.put("msg", "Debe enviar una actividad");
+            responseMap.put("estado", "error");
+        }
+        return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/lab9/repository/ActividadRepository.java
+++ src/main/java/com/example/lab9/repository/ActividadRepository.java
@@ -1,4 +1,9 @@
 package com.example.lab9.repository;
 
-public interface ActividadRepository {
+import com.example.lab9.entity.Actividad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActividadRepository extends JpaRepository<Actividad, Integer> {
 }
